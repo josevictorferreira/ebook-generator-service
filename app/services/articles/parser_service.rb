9bbb9cec6a4255da3a6181455656fda451a2481a
Jose Victor <josevictorferreira@outlook.com>
2023-02-11 10:19:22 -0300

    updated parser and create epub service

--- app/services/articles/parser_service.rb
+++ app/services/articles/parser_service.rb
@@ -20,8 +20,9 @@ module Articles
       Typhoeus.get(url).then do |response|
         return Failure('Unable to fetch article.') if response.failure?
 
-        document = Readability::Document.new(response.response_body, tags: %w[h1 h2 h3 h4 h5 h6 div p img],
-                                                                     attributes: %w[src h1 h2 h3 h4 h5 h6], remove_empty_nodes: false)
+        document = Readability::Document.new(response.response_body, tags: %w[h1 h2 h3 h4 h5 h6 div p img code],
+                                                                     attributes: %w[src h1 h2 h3 h4 h5 h6],
+                                                                     remove_empty_nodes: false)
 
         Success(title: document.title, content: format_content(document.content), cover_image: document.images.first)
       end
@@ -29,7 +30,8 @@ module Articles
 
     def format_content(content)
       content
-        .gsub(/h3|h4|h5|h6/, 'h2')
+        .gsub(/h3|h4/, 'h2')
+        .gsub(/h5|h6/, 'h3')
     end
   end
 end
--- app/services/ebooks/epub/create_service.rb
+++ app/services/ebooks/epub/create_service.rb
@@ -3,6 +3,8 @@
 module Ebooks
   module Epub
     class CreateService
+      attr_reader :title, :content, :cover_image
+
       include ApplicationService
 
       FILE_FORMAT = '.epub'
@@ -13,39 +15,59 @@ module Ebooks
       end
 
       def call(params)
-        generate_epub_file(params)
+        @content = params[:content]
+        @title = params[:title]
+        @cover_image = params[:cover_image]
+        generate_epub_file!
       end
 
       private
 
-      def generate_epub_file(params)
-        gbook = GEPUB::Book.new do |book|
-          book.language = 'en'
-          book.creator = 'J0S3V1'
-          book.add_title params[:title],
-                         title_type: GEPUB::TITLE_TYPE::MAIN,
-                         lang: 'en',
-                         file_as: params[:title],
-                         display_seq: 1
-          cover_image = URI.open(params[:cover_image])
-          book.add_item('img/cover_image.jpg', content: cover_image).cover_image
-          book.ordered do
-            item = book.add_item('text/chap1.xhtml')
-            item.add_content StringIO.new(content_with_title(params))
-            item.toc_text params[:title]
-          end
-        end
-        file = filename(params[:title])
-        content = gbook.generate_epub(file)
+      def book
+        @book ||= GEPUB::Book.new
+      end
+
+      def generate_epub_file!
+        set_file_defaults!
+        set_cover_image!
+        set_file_contents!
+        file = filename(@title)
+        content = book.generate_epub(file)
         Success(filename: file, content:)
+      end
+
+      def set_cover_image!
+        return if @cover_image.blank?
+
+        cover_image_file = URI.open(@cover_image)
+
+        book.add_item('img/cover_image.jpg', content: @cover_image_file).cover_image
+      end
+
+      def set_file_defaults!
+        book.language = 'en'
+        book.creator = 'J0S3V1'
+        book.add_title @title,
+                        title_type: GEPUB::TITLE_TYPE::MAIN,
+                        lang: 'en',
+                        file_as: @title,
+                        display_seq: 1
+      end
+
+      def set_file_contents!
+        book.ordered do
+          item = book.add_item('text/chap1.xhtml')
+          item.add_content StringIO.new(content_with_title)
+          item.toc_text @title
+        end
       end
 
       def filename(file_name)
         "#{file_name.parameterize.underscore}#{FILE_FORMAT}"
       end
 
-      def content_with_title(params)
-        "<h1>#{params[:title]}</h1>#{params[:content]}"
+      def content_with_title
+        "<h1>#{@title}</h1>#{@content}"
       end
     end
   end
